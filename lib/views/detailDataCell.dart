0e0fa460a475bc874f9a24e7dc83a9f83bb5a21e
chlee1252 <chlee1252@gmail.com>
2019-12-02 16:27:15 -0800

    deleted TODO comment

--- lib/views/detailDataCell.dart
+++ lib/views/detailDataCell.dart
@@ -9,7 +9,6 @@ class DetailDataCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Column(
       children: <Widget>[
         new Container(
